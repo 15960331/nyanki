d60e4bbdc61f447edd3ad9482f8401b8553fd441
raiken <raiken@mail.com>
2021-12-25 06:12:20 +0000

    fix: type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/globals.css';
-import type AppProps from 'next/app';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
