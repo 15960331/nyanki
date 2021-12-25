8d101d851640dd0fbf38c224f84bd50c24bda7cf
nyanki <nyanki@example.com>
2021-12-25 07:16:52 +0000

    fix: auto reload dont work on Docker

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // to enable auto reload on Docker
+  // https://github.com/vercel/next.js/issues/6417
+  webpackDevMiddleware: (config) => {
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;
+  }
 }
