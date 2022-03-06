b594c88061ee67243804289e22dc949b86c65a8f
raiken <ra1ken@users.noreply.github.com>
2022-03-06 13:32:25 +0000

    feat: meanings dont appear until it will be opened

--- features/WordReviewCard/WordReviewCard.tsx
+++ features/WordReviewCard/WordReviewCard.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useCallback } from 'react';
+import React, { memo, useCallback, useState } from 'react';
 import { NextPage } from 'next';
 import { Box, Flex, Spinner } from '@chakra-ui/react';
 
@@ -7,17 +7,34 @@ import { Button } from 'components/Button';
 import { useGetRandomWord } from './api/useGetRandomWord';
 
 export const WordReviewCard: NextPage = memo(() => {
+  const [isOpen, setIsOpen] = useState(false);
   const {
     loading, currentWord, getNextWord, reset, remaining,
   } = useGetRandomWord();
 
+  const onClickOpen = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
   const onClickNext = useCallback(() => {
+    setIsOpen(false);
     getNextWord();
   }, [getNextWord]);
 
   const onClickReset = useCallback(() => {
+    setIsOpen(false);
     reset();
   }, [reset]);
+
+  const meaning = () => {
+    if (remaining === 0) {
+      return <Box>🎊🐈🎊</Box>;
+    }
+    if (isOpen) {
+      return <Box>{currentWord?.meaning}</Box>;
+    }
+    return <Box>❔</Box>;
+  };
 
   // TODO: components/Card will have styling props
   return loading
@@ -25,22 +42,33 @@ export const WordReviewCard: NextPage = memo(() => {
     : (
       <Box minWidth="80%">
         <Card
-          title={currentWord?.word}
+          title={remaining === 0
+            ? '🎉You are done, good job!🎉'
+            : currentWord?.word}
           centerText
           darkMode
         >
           <Flex direction="column" gap={4}>
-            {remaining === 0
-              ? <Box>You are done, good job!</Box>
-              : <Box>{currentWord?.meaning}</Box>}
+            {meaning()}
 
             <Flex justifyContent="space-between" gap={4}>
-              <Button
-                onClick={onClickNext}
-                disabled={remaining === 0}
-              >
-                Next
-              </Button>
+              {isOpen
+                ? (
+                  <Button
+                    onClick={onClickNext}
+                    disabled={remaining === 0}
+                  >
+                    Next
+                  </Button>
+                )
+                : (
+                  <Button
+                    onClick={onClickOpen}
+                    disabled={remaining === 0}
+                  >
+                    Open
+                  </Button>
+                )}
               {`remaining: ${remaining}`}
               <Button
                 onClick={onClickReset}
