f0d05501dba9cd6317308b717ea0b074c86353e4
15960331 <84163210+15960331@users.noreply.github.com>
2022-12-13 15:07:30 +0000

    fix: delete popover doesnt close after deleting

---
+++ features/WordForm/components/DeleteButton.tsx
@@ -0,0 +1,59 @@
+import React, { memo, useRef, useState } from 'react';
+import { NextPage } from 'next';
+import { Button } from 'components/Button';
+import { IconButton, Popover, PopoverArrow, PopoverBody, PopoverContent, PopoverFooter, PopoverTrigger, useOutsideClick } from '@chakra-ui/react';
+import { DeleteIcon } from '@chakra-ui/icons';
+
+type Props = {
+  isLoading: boolean;
+  onConfirm: () => void;
+};
+
+export const DeleteButton: NextPage<Props> = memo(({ isLoading, onConfirm }) => {
+  const popoverRef = useRef<HTMLElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  useOutsideClick({
+    ref: popoverRef,
+    handler: () => setIsOpen(false),
+  })
+
+  const handleClick = () => {
+    setIsOpen(true);
+  };
+
+  const handleConfirm = () => {
+    onConfirm();
+    setIsOpen(false);
+  };
+
+  return (
+    <Popover isOpen={isOpen}>
+      <PopoverTrigger>
+        <IconButton
+          colorScheme="red"
+          aria-label="delete item"
+          icon={<DeleteIcon />}
+          roundedLeft={0}
+          onClick={handleClick}
+        />
+      </PopoverTrigger>
+      <PopoverContent ref={popoverRef} color="gray.700">
+        <PopoverArrow />
+        <PopoverBody>
+          Are you sure you want to delete?
+        </PopoverBody>
+        <PopoverFooter>
+          <Button
+            colorScheme="red"
+            size="sm"
+            width="100%"
+            isLoading={isLoading}
+            onClick={handleConfirm}
+          >
+            I am sure!!
+          </Button>
+        </PopoverFooter>
+      </PopoverContent>
+    </Popover>
+  );
+});
